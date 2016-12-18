1bf999d260c7625f73a93b34143be2165baff480
Balázs Hosszu <Balzs Hosszu>
2016-12-18 15:08:38 +0100

    reorganization, introduced testing

---
+++ test/parsing_test.py
@@ -0,0 +1,41 @@
+import unittest
+import src.HumiPlate as humi
+
+class TestParsing(unittest.TestCase):
+
+    def testInt(self):
+        parsed = humi.parseTemp(5)
+        expected =  '5.0*C'
+        self.assertEqual(expected, parsed)
+
+    def testReal(self):
+        parsed = humi.parseTemp(5.1)
+        expected =  '5.1*C'
+        self.assertEqual(expected, parsed)
+
+    def testRealVerbose(self):
+        parsed = humi.parseTemp(5.234)
+        expected =  '5.2*C'
+        self.assertEqual(expected, parsed)
+
+    def testRealMultiDigits(self):
+        parsed = humi.parseTemp(125.2)
+        expected =  '125.2*C'
+        self.assertEqual(expected, parsed)
+
+    def testIntNegative(self):
+        parsed = humi.parseTemp(-10)
+        expected =  '-10.0*C'
+        self.assertEqual(expected, parsed)
+
+    def testIntHumidity(self):
+        parsed = humi.parseHumidity(5)
+        expected =  '5.0*%'
+        self.assertEqual(expected, parsed)
+    
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
