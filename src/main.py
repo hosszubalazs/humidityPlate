8a89b033d196ff8aebca7a0e473f8b4b2776773f
Balázs Hosszu <Balzs Hosszu>
2016-12-18 21:30:56 +0100

    extracted all constants to separate file

--- src/main.py
+++ src/main.py
@@ -4,28 +4,21 @@ import Adafruit_DHT.Adafruit_DHT as dht
 import Adafruit_GPIO.Adafruit_CharLCD as LCDLibrary
 import time
 import DHTDataParser as dataParser
-
+import constants as c
 
 def init_lcd():
     lcd = LCDLibrary.Adafruit_CharLCDPlate()
-    lcd.set_color(0, 0.0, 0.5)
+    lcd.set_color(c.LCD_START_COLOR)
     return lcd
-
-def init_dht():
-    dht_gpio = 4
-    seconds_to_sleep = 4
-    return (dht_gpio, seconds_to_sleep)
-
 
 #If the script is run directly, let's start measurement
 if __name__ == '__main__':
-    DHT_GPIO, SECONDS_TO_SLEEP = init_dht()
     LCD = init_lcd()
 
-    print('Starting data query, every ' + str(SECONDS_TO_SLEEP) + ' seconds')
+    print('Starting data query, every ' + str(c.SECONDS_TO_SLEEP) + ' seconds')
 
     while True:
-        H, T = dht.read_retry(dht.DHT22, DHT_GPIO)
+        H, T = dht.read_retry(dht.DHT22, c.DHT_GPIO)
         H_MSG = 'Humidity='+dataParser.parseHumidity(H)
         T_MSG = 'Temperature='+ dataParser.parseTemp(T)
 
@@ -37,4 +30,4 @@ if __name__ == '__main__':
         LCD.message(H_MSG)
         print(H_MSG)
 
-        time.sleep(SECONDS_TO_SLEEP)
+        time.sleep(c.SECONDS_TO_SLEEP)
