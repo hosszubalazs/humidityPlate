cbef53c645f8e6696f436d99c828ed7f2ac68c5d
root <hbaloo28@gmail.com>
2017-01-29 23:14:45 +0100

    Style fix, mostly whitespaces

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # Python 3
 
+import time
 import Adafruit_DHT as dht
 import Adafruit_GPIO.Adafruit_CharLCD as LCDLibrary
-import time
 import src.DHTDataParser as dataParser
 import src.constants as c
 import src.LCDHelper as lcdHelper
 
 def init_lcd():
     lcd = LCDLibrary.Adafruit_CharLCDPlate()
-    lcd.set_color(c.LCD_COLD_BLUE[0],c.LCD_COLD_BLUE[1],c.LCD_COLD_BLUE[2])
+    lcd.set_color(c.LCD_COLD_BLUE[0], c.LCD_COLD_BLUE[1], c.LCD_COLD_BLUE[2])
     return lcd
 
 #If the script is run directly, let's start measurement
@@ -32,6 +32,6 @@ if __name__ == '__main__':
         LCD.message(H_MSG)
         print(H_MSG)
 
-        targetColor= lcdHelper.map_heat_to_bgcolor(T)
-        LCD.set_color(targetColor[0],targetColor[1],targetColor[2])
+        targetColor = lcdHelper.map_heat_to_bgcolor(T)
+        LCD.set_color(targetColor[0], targetColor[1], targetColor[2])
         time.sleep(c.SECONDS_TO_SLEEP)
